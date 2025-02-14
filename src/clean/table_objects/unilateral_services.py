5784dbcc5eb83fb7fdb597ae424294557b737db0
Elise Jackson <ejackson@holy8a26601.rc.fas.harvard.edu>
2025-02-14 12:08:44 -0500

    merged services df

--- src/clean/table_objects/unilateral_services.py
+++ src/clean/table_objects/unilateral_services.py
@@ -15,6 +15,9 @@ logging.basicConfig(level=logging.INFO)
 
 class UnilateralServices(_AtlasCleaning):
     def __init__(self, year, **kwargs):
+        
+        START_YEAR = 1980
+        
         super().__init__(**kwargs)
         
         self.load_wdi_services_data()
@@ -22,8 +25,9 @@ class UnilateralServices(_AtlasCleaning):
         self.df = self.df.rename(columns={'code': 'iso'})
         self.preprocess_flows("export")
         self.preprocess_flows("import")
-        self.exports = self.isolate_trade_flows("export")
-        self.imports = self.isolate_trade_flows("import")
+        exports = self.isolate_trade_flows("export")
+        imports = self.isolate_trade_flows("import")
+        self.df = exports.merge(imports, on=['year','iso','services'],how='outer',suffixes=('_export','_import'))
         
                 
         
@@ -43,15 +47,21 @@ class UnilateralServices(_AtlasCleaning):
         for col in share:
             val = col.replace('share','value')
             self.df.loc[:, val] = ( self.df[col] / 100 ) * self.df[f'services_{trade_flow}_value']
-        self.df.loc[:,f'unspecified_{trade_flow}_services'] = ((100 - self.df[share].sum(axis=1)) /100) * self.df[f'services_{trade_flow}_value']
-        self.df.loc[self.df[f'unspecified_{trade_flow}_services']<0, f'unspecified_{trade_flow}_services'] = 0
+        self.df.loc[:,f'unspecified_{trade_flow}_value'] = ((100 - self.df[share].sum(axis=1)) /100) * self.df[f'services_{trade_flow}_value']
+        self.df.loc[self.df[f'unspecified_{trade_flow}_value']<0, f'unspecified_{trade_flow}_value'] = 0
         self.df = self.df.drop(columns=share)
         
         
     def isolate_trade_flows(self, trade_flow):
-        cols = [col for col in self.df.columns if f"{trade_flow}" in col and col != f'services_{trade_flow}']
-        return pd.melt(self.df,id_vars=['iso', 'year'],value_vars=cols,var_name='concept',value_name='value')
-        
+        df = self.df.copy()
+        if trade_flow == 'export':
+            cols = [col for col in df.columns if f"{trade_flow}" in col and col != f'services_export_value']
+            renamed_cols = [col.replace(f'_export_value', '') for col in cols]
+        else:
+            cols = [col for col in df.columns if f"{trade_flow}" in col and col != f'services_import_value']
+            renamed_cols = [col.replace(f'_import_value', '') for col in cols]
+        df = df.rename(columns=dict(zip(cols, renamed_cols)))
+        return pd.melt(df,id_vars=['iso', 'year'],value_vars=renamed_cols,var_name='services',value_name='value')
                               
                     
         
